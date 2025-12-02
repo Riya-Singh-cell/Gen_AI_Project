fbcf78fe866039ec711b38703cc239a41d719e05
Aadarsh Senapati <57413192+aadarshsenapati@users.noreply.github.com>
2025-12-03 00:09:40 +0530

    Implement text cleaning function for keyword extraction
    Added a clean_text function to preprocess input text before keyword extraction.

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from model.keyword_extractor import extract_keywords_tfidf
+import re
+
 
 app = Flask(__name__)
 CORS(app)
@@ -13,6 +15,30 @@ def extract():
         return jsonify({"error": "No text provided"}), 400
     keywords = extract_keywords_tfidf(text)
     return jsonify({"keywords": keywords})
+
+def clean_text(text):
+    text = re.sub(r'https?://\S+|www\.\S+', '', text)
+    text = re.sub(r'[^a-zA-Z\s]', ' ', text)
+    text = re.sub(r'\s+', ' ', text).strip()
+    words = [w for w in text.split() if len(w) > 2]
+
+    return " ".join(words)
+
+
+@app.route('/api', methods=['POST'])
+def api():
+    data = request.get_json()
+    text = data.get("text", "")
+
+    if not text.strip():
+        return jsonify({"error": "No text provided"}), 400
+
+    cleaned = clean_text(text)        
+    n = data.get("n", 10)
+    keywords = extract_keywords_tfidf(cleaned,n)
+
+    return jsonify({"keywords": keywords})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
